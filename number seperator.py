7417f7b08eec171bc7bc0424aeb52dc0aaa7dbe1
dhruv681 <65502877+dhruv681@users.noreply.github.com>
2020-05-26 20:00:48 +0100

    Add files via upload

--- number seperator.py
+++ number seperator.py
@@ -1,7 +1,8 @@
-num= (input('type random amount of letters and words but put spaces in the middle:'))
+num= list(input('type random amount of letters and words and i will put out the numbers:'))
 
 numb=[]
-for word in num.split():
+for word in num:
     if word.isdigit():
         numb.append(int(word))
 print('so your numbers are ',numb)
+
